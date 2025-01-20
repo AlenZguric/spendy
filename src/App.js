d981f91f6bcf7502127b2569fe98297047aec9c1
Alen Zgurić <zguric.alen@hotmail.com>
2025-01-20 23:36:00 +0100

    Added Squares animated component

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import UsersPage from "./pages/UsersPage";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 
+//import Squares from "../src/utils/scss/Squares/Squares";
+
 function App() {
   const [user, setUser] = useState(null);
   const [isFetching, setIsFetching] = useState(true);
@@ -40,28 +42,75 @@ function App() {
   }
   //console.log("Korisnik u App.js:", user);
 
-
   return (
     <div className="App">
+      {/* <Squares
+        speed={0.5}
+        squareSize={40}
+        direction="diagonal"
+        borderColor="#fff"
+        hoverFillColor="#222"
+      /> */}
       <BrowserRouter
         future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
       >
         <NavBar />
 
         <Routes>
-          <Route path="/" element={<HomePage  />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<Contactpage user={user} />} />
           <Route path="/sign-in" element={<SignIn user={user} />} />
           <Route path="/sign-up" element={<SignUp user={user} />} />
 
-
-          <Route path="/dashboard" element={<ProtectedRoute user={user}> <DashBoard userId={user?.uid} /></ProtectedRoute>}/>
-          <Route path="/add-expense" element={<ProtectedRoute user={user}><AddExpensePage /></ProtectedRoute>} />
-          <Route path="/expense-list" element={<ProtectedRoute user={user}><ExpenseListPage /></ProtectedRoute>}  />
-          <Route path="/category-expenses" element={<ProtectedRoute user={user}><CategoryExpensesPage /></ProtectedRoute>} />
-          <Route path="/savings" element={<ProtectedRoute user={user}><SavingsPage /></ProtectedRoute>} />
-          <Route path="/users" element={<ProtectedRoute user={user}><UsersPage /></ProtectedRoute>} />
-
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute user={user}>
+                {" "}
+                <DashBoard userId={user?.uid} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/add-expense"
+            element={
+              <ProtectedRoute user={user}>
+                <AddExpensePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/expense-list"
+            element={
+              <ProtectedRoute user={user}>
+                <ExpenseListPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/category-expenses"
+            element={
+              <ProtectedRoute user={user}>
+                <CategoryExpensesPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/savings"
+            element={
+              <ProtectedRoute user={user}>
+                <SavingsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute user={user}>
+                <UsersPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
